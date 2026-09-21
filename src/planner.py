e1fe133bdd0e21d84c8d9ecf32ea4a8af50bc6fb
benchchung <bchung28@calpoly.edu>
2026-09-20 23:37:41 -0700

    Implement BFS in planner

--- src/planner.py
+++ src/planner.py
@@ -11,5 +11,10 @@ def bfs_search(start, goal, available_actions, apply_action):
         # TODO: for each action available from `state`, compute the next
         # state, and if it hasn't been visited, add it to the frontier
         # with the updated path.
-        ...
+        for action in available_actions(state): #checks for all the potential actions you can do
+            next_state = apply_action(state, action) #creates the next state, and can only choose actions that have prerequisites met
+            if next_state not in visited:
+                visited.add(next_state) #adding the new state to visited to avoid repeats.
+                frontier.append((next_state, path + [action])) # queue the new state along with the route that reached it
+
     return None  # no valid sequence exists
