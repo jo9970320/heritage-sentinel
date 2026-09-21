2d03d8204b66e0055e9a64c2e2b2c5c2452f0198
benchchung <bchung28@calpoly.edu>
2026-09-21 08:46:59 -0700

    Add selector for switching between problems

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
-from restoration_graph import START, GOAL, available_actions, apply_action
+import restoration_graph
+import taco_graph
 from planner import bfs_search
 
+problem = taco_graph #change this statement to which graph you're testing
+
 if __name__ == "__main__":
-    plan = bfs_search(START, GOAL, available_actions, apply_action)
-    print("Restoration plan:", plan)
+    plan = bfs_search(problem.START, problem.GOAL,
+                      problem.available_actions, problem.apply_action)
+    print("Plan:", plan)
